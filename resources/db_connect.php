512209b227f6eb679bae932b76779171cf28cb6f
hawthorneben <hawthorneben@gmail.com>
2017-12-24 13:49:58 -0700

    Removed credentials

--- resources/db_connect.php
+++ resources/db_connect.php
@@ -1,7 +1,7 @@
 <?php
     // Connecting, selecting database
-    $dbUsername = 'bitnami';
-    $dbPassword = 'bitnami';
+    $dbUsername = '';
+    $dbPassword = '';
     try {
         $connection = new PDO("pgsql:dbname='ladder' host='localhost' password='$dbPassword' user='$dbUsername'",
                    $dbUsername, $dbPassword, array( PDO::ATTR_PERSISTENT => true));
